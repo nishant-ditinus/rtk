12d24cac595377710bf7bf3cfb7055646e186829
Nishant Singh Yadav <159749446+nishant-ditinus@users.noreply.github.com>
2024-03-11 13:36:23 +0000

    Error correction

--- src/components/AddTodo.jsx
+++ src/components/AddTodo.jsx
@@ -26,9 +26,7 @@ function AddTodo() {
       <button
         type="submit"
         className="text-white bg-indigo-500 border-0 py-2 px-6 focus:outline-none hover:bg-indigo-600 rounded text-lg"
-      >
-        Add Todo
-      </button>
+      >Add Todo</button>
     </form>
   )
 }
